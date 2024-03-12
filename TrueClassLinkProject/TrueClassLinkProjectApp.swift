9605b9d202b18263533dbb305b82eac118c66a44
Filip <fmilenkovic03@gmail.com>
2024-03-12 13:56:10 +0100

    Initial Commit

---
+++ TrueClassLinkProject/TrueClassLinkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueClassLinkProjectApp.swift
+//  TrueClassLinkProject
+//
+//  Created by Apprenant98 on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrueClassLinkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
