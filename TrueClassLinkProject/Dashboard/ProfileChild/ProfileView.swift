49491c6e1334bc0538feb4ec7957f65c92984e33
Filip <fmilenkovic03@gmail.com>
2024-03-18 09:10:57 +0100

    Un peu de tout

--- TrueClassLinkProject/Dashboard/ProfileChild/ProfileView.swift
+++ TrueClassLinkProject/Dashboard/ProfileChild/ProfileView.swift
@@ -48,93 +48,16 @@ struct AnnaelStudentProfileView: View {
                             }
                             .padding(.bottom, 20)
     
-}
-}
+                        }
+                    }
                 }
-                
-                
-                                
-    }
             }
-       
         }
+    }
     
-
 
 struct AnnaelStudentProfileView_Preview : PreviewProvider {
     static var previews: some View {
         AnnaelStudentProfileView(profileEditMode: true, profilePictureExists: true, eleve: studentProfiles[0])
     }
 }
-
-
-
-    
-//VStack(alignment: .leading) { //page
-//    HStack {
-//        Spacer()
-//        ProfilePicture(profileEditMode: $profileEditMode, profilePictureExists: $profilePictureExists)
-//        ProfileText()
-//            .padding(.leading, 10)
-//        if ownProfile == true {
-//            ProfileEditButton(profileEditMode: $profileEditMode)
-//                .padding(.leading, 35)
-//                Spacer()
-//        } else {
-//            DMButton()
-//                .padding(.leading, 35)
-//                Spacer()
-//        }
-//    } //bouton DM ou ProfileEdit
-//    .padding(.top, 50)
-//    .padding(.bottom, 30)
-//
-//    HStack {
-//        Spacer()
-//        VStack(alignment: .leading) {
-//            Text("Loisirs")
-//                .font(.title2)
-//                .fontWeight(.semibold)
-//                .foregroundStyle(.orangeEdu)
-//                .padding(.bottom, -5)
-//
-//            ZStack {
-//                ProfileInfosRectangle()
-//                    LazyVGrid(columns: Array(repeating: GridItem(), count: 3), spacing: 5) {
-//                        ForEach(studentProfiles[0].hobby ?? [], id: \.self) { hobby in
-//                            VStack {
-//                                Image(systemName: hobby.hobbyIcon)
-//                                    .foregroundStyle(.purpleEdu)
-//                                Text(hobby.hobbyName)
-//                                    .fontWeight(.light)
-//                                    .font(.caption2)
-//                                    .multilineTextAlignment(.center)
-//                                    .foregroundStyle(.purpleEdu)
-//                                    .frame(width: 60)
-//                            }
-//                            .padding(.bottom, 20)
-//                        }
-//                        AddHobbiesIcon(profileEditMode: $profileEditMode, currentProfile: studentProfiles[0], showingHobbiesList: false)}
-//            }
-//            .padding(.bottom, 40)
-//
-//            VStack { //pour test si append() marche
-//                ForEach(studentProfiles[0].hobby ?? [], id: \.self) { hobby in
-//                    Text(hobby.hobbyName)}
-//            }
-//
-//
-//            Text("En ce moment ...")
-//                .font(.title2)
-//                .fontWeight(.semibold)
-//                .foregroundStyle(.orangeEdu)
-//            .padding(.bottom, -5)
-//            ProfileInfosRectangle()
-//        }
-//        Spacer()
-//    }
-//
-//        .padding(.bottom, 30)
-//
-//    Spacer()
-//}
