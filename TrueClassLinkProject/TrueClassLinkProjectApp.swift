14c35515eb0ef0adb76671f916c852ef27692897
Filip <fmilenkovic03@gmail.com>
2024-03-20 16:26:53 +0100

    on rend fonctionnel

--- TrueClassLinkProject/TrueClassLinkProjectApp.swift
+++ TrueClassLinkProject/TrueClassLinkProjectApp.swift
@@ -14,6 +14,8 @@ struct TrueClassLinkProjectApp: App {
         WindowGroup {
             InscriptionConnexion(hist: MessageHistorique(), eleveList: EleveList())
                 .environmentObject(eleveList)
+                .environmentObject(UserChoice())
+                
         }
     }
 }
